768901e87d0d31aab1e76d9720109b917f7e3d1f
K0ppai <psthu.koppai@gmail.com>
2023-10-25 22:22:03 +0630

    Update associations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,5 @@
 class City < ApplicationRecord
+  has_many :reservations
+  has_many :yachts, through: :yacht_cities
+  has_many :yacht_cities
 end
--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,2 +1,5 @@
 class Yacht < ApplicationRecord
+  has_many :reservations
+  has_many :cities, through: :yacht_cities
+  has_many :yacht_cities
 end
