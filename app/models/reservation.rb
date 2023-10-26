3746e2c8f461656664c069f9cffa770c383dd4fc
Yout <youtlone95@gmail.com>
2023-10-26 10:38:51 +0630

    Implement reservation model validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,11 @@ class Reservation < ApplicationRecord
   belongs_to :yacht
   belongs_to :user
   belongs_to :city
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :total_value, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :yacht_id, presence: true
+  validates :user_id, presence: true
+  validates :city_id, presence: true
 end
