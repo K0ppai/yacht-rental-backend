a1cff1cf5ae3e4913a69c94d6a72576b9364ae74
Yout <youtlone95@gmail.com>
2023-10-26 10:37:09 +0630

    Implement yacht model validation

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -2,4 +2,10 @@ class Yacht < ApplicationRecord
   has_many :reservations
   has_many :cities, through: :yacht_cities
   has_many :yacht_cities
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, presence: true
+  validates :model, presence: true, length: { maximum: 255 }
+  validates :image, presence: true
+  validates :fee_per_day, presence: true, numericality: { greater_than: 0 }
 end
